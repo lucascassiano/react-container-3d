55551d6f0bbfcd333051369a414f74819f4c11d3
Lucas Cassiano Silva <lucascassiano@dhcp-18-189-125-32.dyn.MIT.EDU>
2017-11-13 23:18:59 -0500

    test

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,4 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Button from './index';
 
-it('renders without crashing', () => {
-  const wrapper = shallow(<Button>Hello Button</Button>);
-  expect(wrapper.text()).toEqual('Hello Button');
-});
+
