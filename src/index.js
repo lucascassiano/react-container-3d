d41331142f026ab4451b0867ac121f372a311bdb
Lucas Cassiano Silva <cassiano@mit.edu>
2017-12-19 01:26:45 -0300

    better hover

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import * as THREE from "three";
@@ -27,16 +25,17 @@ let mouse = new THREE.Vector2(),
   INTERSECTED;
 
 //for css rendering
-let renderer2d, scene2d, canvas2d;
+let scene2d, canvas2d;
 
 class Container3d extends Component {
   constructor(props) {
     super(props);
     this.onHoverStart = this.onHoverStart.bind(this);
     this.onHoverEnd = this.onHoverEnd.bind(this);
-    //this.onHover = this.onHover.bind(this);
+
     this.onError = this.onError.bind(this);
     this.onDocumentMouseMove = this.onDocumentMouseMove.bind(this);
+    this.clearScene = this.clearScene.bind(this);
   }
 
   componentDidMount() {
@@ -63,11 +62,7 @@ class Container3d extends Component {
   }
 
   getSize() {
-    var {
-      width,
-      percentageWidth,
-      aspect
-    } = this.props;
+    var { width, percentageWidth, aspect } = this.props;
     if (percentageWidth)
       width = window.innerWidth * parseFloat(percentageWidth) / 100;
 
@@ -94,7 +89,18 @@ class Container3d extends Component {
   }
 
   clearScene() {
+    if (scene != undefined)
+      scene.traverse(function(object) {
+        scene.remove(object);
+        if (object.geometry) object.geometry.dispose();
+        if (object.material) object.material.dispose();
+        //object.dispose();
+      });
+
+    var canvas = this.refs.threeCanvas;
     scene = new THREE.Scene();
+    //this._createScene(canvas);
+    this.reloadScene(scene);
   }
 
   updateDimensions() {
@@ -131,13 +137,11 @@ class Container3d extends Component {
     }
 
     var canvas = this.refs.threeCanvas;
-    //var canvas2d = this.refs.cssCanvas;
 
     canvas.height = height;
-    canvas2d.height = height;
 
     renderer.setSize(width, height);
-    renderer2d.setSize(width, height);
+
     camera.aspect = width / height;
 
     camera.updateProjectionMatrix();
@@ -145,10 +149,7 @@ class Container3d extends Component {
 
   init() {
     //this.props.onHover("hello");
-    const {
-      width,
-      height
-    } = this.getSize();
+    const { width, height } = this.getSize();
 
     const canvas = this.refs.threeCanvas;
     //const canvas2d = this.refs.cssCanvas;
@@ -168,58 +169,20 @@ class Container3d extends Component {
       alpha: true,
       opacity: 0.5
     });
-    //renderer.setClearColor(0xffffff, 1);
 
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-    renderer2d = new THREE.CSS3DRenderer({
-      canvas: canvas2d
-    });
-
-    renderer2d.setSize(renderer.width, renderer.height);
-    scene2d = new THREE.Scene();
-    //test 2d
-    //HTML
-    var element = document.createElement("div");
-    element.innerHTML = "Plain text inside a div.";
-    element.style.background = "#0094ff";
-    element.style.fontSize = "2em";
-    element.style.color = "white";
-    element.style.padding = "2em";
-
-    //CSS Object
-    var div = new THREE.CSS3DObject(element);
-    div.position.x = 0;
-    div.position.y = 0;
-    div.position.z = 0;
-    div.scale.x = 0.1;
-    div.scale.y = 0.1;
-
-    // scene2d.add(div);
-
-    renderer2d.domElement.style.position = "absolute";
-    renderer2d.domElement.style.top = 0;
-    renderer2d.domElement.style.zIndex = 1;
-
-    var node = ReactDOM.findDOMNode(this.refs.cssCanvas);
-    canvas2d = this.refs.cssCanvas.appendChild(renderer2d.domElement);
-
-    //end test 2d
-
-    this._createScene(canvas, canvas2d);
+    this._createScene(canvas);
     var _this = this;
 
-    this._render = function () {
+    this._render = function() {
       //animation = requestAnimationFrame(_this._render);
-      setTimeout(function () {
+      setTimeout(function() {
         requestAnimationFrame(_this._render);
       }, 1000 / 30); //running @ 30FPS
 
-      var {
-        phi,
-        theta
-      } = _this.props;
+      var { phi, theta } = _this.props;
 
       if (phi && theta && controls) {
         controls.setPolarAngle(phi);
@@ -251,18 +214,22 @@ class Container3d extends Component {
           if (INTERSECTED != intersects[0].object) {
             if (INTERSECTED) {
               _this.onHoverEnd(INTERSECTED, scene, camera, renderer);
+              INTERSECTED = intersects[0].object;
+              _this.onHoverStart(INTERSECTED, scene, camera, renderer);
             } else {
               INTERSECTED = intersects[0].object;
               _this.onHoverStart(INTERSECTED, scene, camera, renderer);
             }
           }
         } else {
-          if (INTERSECTED)
+          if (INTERSECTED) {
             _this.onHoverEnd(INTERSECTED, scene, camera, renderer);
+            INTERSECTED = null;
+          }
           INTERSECTED = null;
         }
       }
-      renderer2d.render(scene2d, camera);
+
       renderer.render(scene, camera);
       // this.updateRendererDimensions();
     };
@@ -316,7 +283,7 @@ class Container3d extends Component {
       enablePan
     } = this.props;
 
-    if (addGrid != undefined ? addGrid : true) {
+    if (addGrid ? addGrid : false) {
       var gridXZ = new THREE.GridHelper(20, 20);
       gridXZ.name = "grid";
       scene.add(gridXZ);
@@ -331,7 +298,7 @@ class Container3d extends Component {
       scene.add(plane);
     }
 
-    if (addLight != undefined ? addLight : true) {
+    if (addLight != undefined ? addLight : false) {
       scene.add(new THREE.AmbientLight(0xf0f0f0));
       var light = new THREE.SpotLight(0xffffff, 1.5);
       light.position.set(50, 50, 50);
@@ -339,6 +306,7 @@ class Container3d extends Component {
       light.shadow = new THREE.LightShadow(
         new THREE.PerspectiveCamera(70, 1, 10, 1000)
       );
+
       light.shadow.bias = -0.000222;
       light.shadow.mapSize.width = 1024;
       light.shadow.mapSize.height = 1024;
@@ -356,8 +324,8 @@ class Container3d extends Component {
   }
 
   //Insert all 3D elements here
-  _createScene(canvas, canvas2d) {
-    console.log(this.props);
+  _createScene(canvas) {
+    //console.log(this.props);
 
     const {
       addControls,
@@ -417,7 +385,7 @@ class Container3d extends Component {
   }
 
   onError(error) {
-    if (this.props) {
+    if (this.props.onError) {
       this.props.onError(error);
     }
   }
@@ -443,17 +411,10 @@ class Container3d extends Component {
     let style1 = {
       zIndex: 5
     };
-    return ( <
-      div ref = "rootthree" >
-      <
-      canvas ref = "threeCanvas"
-      style = {
-        style1
-      }
-      /> <
-      div ref = "cssCanvas" / >
-      <
-      /div>
+    return (
+      <div ref="rootthree">
+        <canvas ref="threeCanvas" style={style1} /> <div ref="cssCanvas" />
+      </div>
     );
   }
 }
--- stories/index.js
+++ stories/index.js
@@ -8,6 +8,14 @@ import "./../src/css/style.css";
 
 let cube;
 let t = 0;
+let mousePos = { x: 0, y: 0 };
+
+window.addEventListener("mousemove", function(event) {
+  var x = event.x;
+  var y = event.y;
+  mousePos = { x, y };
+  //console.log(mousePos);
+});
 
 function setup(scene, camera, renderer) {
   var geometry = new THREE.BoxGeometry(5, 5, 5);
@@ -16,27 +24,122 @@ function setup(scene, camera, renderer) {
   scene.add(cube);
 }
 
+function setup2(scene, camera, renderer) {
+  var geometry = new THREE.SphereGeometry(3, 25, 25);
+  var material = new THREE.MeshBasicMaterial({ color: 0xf1c40f });
+  var sun = new THREE.Mesh(geometry, material);
+  sun.position.y = 10;
+  sun.position.z = -10;
+
+  scene.add(sun);
+
+  var geometry2 = new THREE.BoxGeometry(18, 1, 18);
+  var material2 = new THREE.MeshLambertMaterial({ color: 0x2ecc71 });
+  var cube2 = new THREE.Mesh(geometry2, material2);
+  cube2.position.y = -3.5;
+
+  scene.add(cube2);
+  cube2.castShadow = true; //default is false
+  cube2.receiveShadow = true; //default
+  cube2.material.shadowOpacity = 0.6;
+
+  //light
+  var light = new THREE.PointLight(0xf1c40f, 1, 100);
+  light.position.set(sun.position.x, sun.position.y, sun.position.z);
+
+  light.castShadow = true;
+  light.shadow.darkness = 0.5;
+  scene.add(light);
+
+  scene.add(new THREE.AmbientLight(0x555555));
+
+  var dir = new THREE.Vector3(
+    -sun.position.x,
+    -sun.position.y,
+    -sun.position.z
+  );
+
+  //normalize the direction vector (convert to vector of length 1)
+  dir.normalize();
+
+  var origin = new THREE.Vector3(
+    sun.position.x,
+    sun.position.y,
+    sun.position.z
+  );
+  //origin.normalize();
+  var length = 8;
+  var hex = 0xf1c40f;
+
+  var arrowHelper = new THREE.ArrowHelper(dir, origin, length, hex);
+  scene.add(arrowHelper);
+
+  //light.shadowDarkness = 0.5;
+
+  //trees
+  var trees = Math.random() * 20 + 8;
+  for (var i = 0; i < trees; i++) {
+    var tree_geometry = new THREE.ConeGeometry(2, 5, 6);
+    var tree_material = new THREE.MeshLambertMaterial({ color: 0x16a085 });
+    tree_material.shadowOpacity = 0.6;
+    var cone = new THREE.Mesh(tree_geometry, tree_material);
+    cone.position.x = Math.random() * 10 - 5;
+    cone.position.z = Math.random() * 10 - 5;
+    cone.position.y = 0.6;
+    scene.add(cone);
+    cone.castShadow = true; //default is false
+    cone.receiveShadow = true; //default
+    var trunk = new THREE.Mesh(
+      new THREE.CylinderGeometry(0.6, 0.6, 2, 6),
+      new THREE.MeshLambertMaterial({ color: 0x443322 })
+    );
+    //trunk.position.x = cone.position.x;
+    //trunk.position.z = cone.position.z;
+    trunk.position.y = -3;
+
+    cone.add(trunk);
+    cone.objectType = "tree";
+  }
+}
+
 function update(scene, camera, renderer) {}
 
 function onHoverStart(obj) {
-  console.log("begin hover object", obj.material);
+  obj.originalColor = obj.material.color;
+  obj.material = new THREE.MeshBasicMaterial({ color: 0xf39c12 });
+}
 
-  obj.originalColor = obj.material.color.getHex();
-  obj.material.color.setHex(0xff0000);
-  if (obj instanceof THREE.Mesh) {
-    console.log("is mesh");
-    
+function onHoverTreesStart(obj, scene) {
+  console.log("begin hover object", obj);
+  if (obj.objectType == "tree") {
+    obj.originalColor = obj.material.color;
+    var outlineMaterial1 = new THREE.MeshBasicMaterial({
+      color: 0xff0000,
+      side: THREE.BackSide
+    });
 
-    //obj.material.color = "#ff0000";
+    obj.material = new THREE.MeshBasicMaterial({
+      color: 0xf39c12
+    });
+    obj.children[0].material = new THREE.MeshBasicMaterial({ color: 0xf39c12 });
   }
+}
 
-  //action("hover");
-  //action('button-click');
+function onHoverTreesEnd(obj) {
+  if (obj.objectType == "tree") {
+    obj.material = new THREE.MeshLambertMaterial({ color: obj.originalColor });
+    obj.material.castShadow = true;
+    obj.material.receiveShadow = true;
+    obj.children[0].material = new THREE.MeshLambertMaterial({
+      color: 0x443322
+    });
+  }
 }
 
 function onHoverEnd(obj) {
   console.log("end hovering object", obj);
-  if (obj.originalColor) obj.material.color.setHex(obj.originalColor);
+  if (obj.originalColor)
+    obj.material = new THREE.MeshBasicMaterial({ color: obj.originalColor });
 }
 
 storiesOf("Button", module)
@@ -99,6 +202,44 @@ storiesOf("Button", module)
         addGrid={false}
         setup={setup}
         update={update}
+      />
+    </div>
+  ))
+
+  .add("Multiple objects", () => (
+    <div className="canvas-3d">
+      <Container3d
+        marginTop={30}
+        aspect={16 / 9}
+        percentageWidth={"100%"}
+        fitScreen
+        key={"d"}
+        marginBottom={110}
+        enableZoom={false}
+        onHoverStart={onHoverTreesStart}
+        onHoverEnd={onHoverTreesEnd}
+        addGrid={false}
+        setup={setup2}
+        update={update}
+        addLight={false}
+      />
+    </div>
+  ))
+
+  .add("Removed Lights", () => (
+    <div className="canvas-3d">
+      <Container3d
+        marginTop={30}
+        aspect={16 / 9}
+        percentageWidth={"100%"}
+        fitScreen
+        marginBottom={110}
+        onHoverStart={onHoverTreesStart}
+        onHoverEnd={onHoverTreesEnd}
+        addGrid={false}
+        setup={setup2}
+        update={update}
+        addLight={false}
       />
     </div>
   ));
